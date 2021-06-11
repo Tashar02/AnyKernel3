53a82a360e309d7335246db3d1665c4c44ee6c2f
Tashar02 <tashfinshakeerrhythm@gmail.com>
2021-06-11 23:13:54 +0600

    Import files for packing up Tempest-4.19 kernel
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,42 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Tempest 4.19 Edition
+dev.string=Tashar
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=jasmine_sprout
+device.name2=wayne
+supported.versions=9 - 12
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
+ramdisk_compression=auto;
+no_block_display=1;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+## Begin AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+# end ramdisk changes
+
+write_boot;
+## End AnyKernel install
