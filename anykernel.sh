bf24fa7f0757b3da915fce174e81948e2bd382a6
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-05-01 04:58:28 +0600

    anykernel: Add device and kernel info
    - Additionally, rewrite the anykernel script
      and add prompt changes.
    - Kernel is for lavender, tulip, jasmine_sprout, wayne, whyred.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,24 +4,24 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-kernel.for=KernelForDriver
-kernel.compiler=AtomSemx
-kernel.made=Kunmun
-kernel.version=4.19.xxx
-message.word=bruh
-build.date=2069
+kernel.string=Scarlet-X
+kernel.for=EAS
+kernel.compiler=Neutron Clang 15
+kernel.made=Tashar
+kernel.version=4.19.240
+message.word=Thank you for installing Scarlet-X
+build.date=$(date)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
+device.name1=lavender
+device.name2=tulip
+device.name3=jasmine_sprout
+device.name4=wayne
+device.name5=whyred
+supported.versions=9 - 13
 supported.patchlevels=
 '; } # end properties
 
@@ -45,43 +45,6 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 ## AnyKernel boot install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
-
 write_boot;
 ## end boot install
-
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
-
+## End AnyKernel install
